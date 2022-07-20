ffc1d6457b1e6371be1ebc2e8842bef1df1467c1
PiXeleum <jeffreyrigaux@gmail.com>
2022-07-20 15:17:11 +0200

    add css in navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <header>
-      <nav className="flex items-center justify-center text-xs text-zinc-50 bg-slate-900">
+      <nav className="flex items-center justify-center text-xs text-zinc-50 bg-slate-900 p-3 space-x-4">
         <Link to="/Home">Home</Link>
         <Link to="/Project">Project</Link>
         <Link to="/Profile">Profile</Link>
