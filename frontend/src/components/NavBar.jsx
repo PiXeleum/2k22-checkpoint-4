12122750628be885639799dbec632977c270a868
PiXeleum <jeffreyrigaux@gmail.com>
2022-07-20 15:17:46 +0200

    add css tailwind in navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -3,10 +3,16 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <header>
-      <nav className="flex items-center justify-center text-xs text-zinc-50 bg-slate-900 p-3 space-x-4">
-        <Link to="/Home">Home</Link>
-        <Link to="/Project">Project</Link>
-        <Link to="/Profile">Profile</Link>
+      <nav className="flex items-center justify-center text-xs text-zinc-50 bg-slate-900 p-2 ">
+        <Link className=" hover:bg-slate-400 p-2" to="/Home">
+          Home
+        </Link>
+        <Link className=" hover:bg-slate-400 p-2" to="/Project">
+          Project
+        </Link>
+        <Link className=" hover:bg-slate-400 p-2" to="/Profile">
+          Profile
+        </Link>
       </nav>
     </header>
   );
