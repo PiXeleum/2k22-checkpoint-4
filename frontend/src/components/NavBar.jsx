c01888df9be931ff19497a395e0d65d9e913644d
PiXeleum <jeffreyrigaux@gmail.com>
2022-07-20 15:02:43 +0200

    corrected bug tailwind

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 
 export default function NavBar() {
   return (
-    <header className="App-header">
-      <nav>
-        <Link to="/Home">azerty</Link>
-        <Link to="/Project">qsdfgh</Link>
-        <Link to="/Profile">wxcvbn</Link>
+    <header>
+      <nav className="flex items-center justify-center text-xs text-zinc-50 bg-slate-900">
+        <Link to="/Home">Home</Link>
+        <Link to="/Project">Project</Link>
+        <Link to="/Profile">Profile</Link>
       </nav>
     </header>
   );
